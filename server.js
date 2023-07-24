675d6494f22c84ab95e605e396d54ada94fe51e3
Ankesh Kumar <ankeshkumar@yatharthriti.com>
2023-07-24 18:43:34 +0530

    24-07-2023 evening

--- server.js
+++ server.js
@@ -1,59 +1,89 @@
 import express from 'express';
-import mysql from 'mysql2';
-import bodyParser from 'body-parser';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import multer from 'multer';
-import path from 'path';
+import mysql from 'mysql';
+import path from 'path'; // Import the 'path' module
 
 const app = express();
-const port = 4000;
-app.use(express.json());
-app.use(cors());
+const port = process.env.PORT || 4000;
 app.use(bodyParser.json());
 
-// Create MySQL connection pool instead of a single connection
-const connection = mysql.createPool({
+app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    // Use the original filename of the uploaded image
+    const uniqueSuffix = Date.now() + path.extname(file.originalname);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
+  }
+});
+const upload = multer({ storage: storage });
+
+
+// MySQL database connection
+const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
   database: 'my-vue-app',
 });
 
-// Test the connection to MySQL
-connection.getConnection((err, conn) => {
+// Connect to the database
+db.connect((err) => {
   if (err) {
     console.error('Error connecting to MySQL database:', err);
     return;
   }
-  conn.release();
-  console.log('Connected to MySQL database');
+  console.log('Connected to MySQL database.');
 });
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, 'thumbnails'));
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-const upload = multer({ storage });
 
-app.post('/api/products', upload.single('thumbnail'), (req, res) => {
-  const { title, description, price } = req.body;
-  const thumbnail = req.file;
+app.post("/api/products", upload.single('thumbnail'), (request, response) => {
+  const { title, description, price } = request.body;
+  const thumbnail = request.file.filename; // Use the generated unique filename
+
+  // Validate input data (you can add more validation here)
+  if (!title || !description || !price || !thumbnail) {
+    return response.status(400).json({ error: 'All fields are required.' });
+  }
+
+  // Insert the new product data into the 'products' table
   const sql = 'INSERT INTO products (title, description, price, thumbnail) VALUES (?, ?, ?, ?)';
-  connection.query(sql, [title, description, price, thumbnail], (err, result) => {
+  const values = [title, description, price, thumbnail];
+
+  db.query(sql, values, (err, result) => {
     if (err) {
-      console.error('Error adding product:', err);
-      res.status(500).json({ error: 'Error adding product', details: err.message });
-    } else {
-      console.log('Product added:', result.insertId);
-      res.status(200).send('Product added successfully');
+      console.error('Error inserting product into the database:', err);
+      return response.status(500).json({ error: 'Error inserting product into the database.' });
     }
+    // Return the ID of the newly created product
+    return response.status(201).json({ id: result.insertId, message: 'Product created successfully.' });
+  });
+});
+
+
+
+
+
+// GET API to fetch all products
+app.get("/api/products", (request, response) => {
+  // Retrieve all products from the 'products' table
+  const sql = 'SELECT * FROM products';
+
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.error('Error fetching products from the database:', err);
+      return response.status(500).json({ error: 'Error fetching products from the database.' });
+    }
+    // Return the fetched products
+    return response.status(200).json(result);
   });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log('I am live again');
 });
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useEffect } from 'react';
+
 
 function Login() {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [price, setPrice] = useState('');
   const [thumbnail, setThumbnail] = useState(null);
+  const [daata,setdata]=useState("")
+  
+
+  const [fetchProductData, setfetchProductData] = useState([]);
 
 
-  const handleSubmit = (e) => {
+  const handleSubmit =  async (e) => {
+    setdata('g')
     e.preventDefault();
     const productData = {
       title, 
@@ -16,28 +23,84 @@ function Login() {
       price,
       thumbnail
     };
-    console.log(productData)
-    axios.post('http://localhost:4000/api/products', productData)
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+  
+    axios.post('http://localhost:4000/api/products', productData, { 
+      headers: {
+        "Content-Type": "multipart/form-data",
+      }
+    })
+    .then((res) => {
+      console.log(res.data);
+      setTitle('');
+      setDescription('');
+      setPrice('');
+      setThumbnail('');
+    })
+    .catch((err) => {
+      console.error(err);
+    });
   };
 
 
+
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("http://localhost:4000/api/products");
+        const data = await response.json();
+        setfetchProductData(data);
+        console.log(data)
+
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchData();
+    
+  }, [daata]);
+
   return (
-    <div>
-      <h1>Add Product</h1>
-      <form method="POST" onSubmit={handleSubmit} encType='multipart/form-data'>
-        <input type="text" placeholder="Title" value={title} onChange={(e)=>setTitle(e.target.value)}/>
-        <input type="text" placeholder="Description" value={description} onChange={(e)=>setDescription(e.target.value)}/>
-        <input type="number" placeholder="Price" value={price} onChange={(e)=>setPrice(e.target.value)}/>
-        <input type="file" placeholder="Thumbnail"  onChange={(e)=>setThumbnail(e.target.files[0])}/>
-        <button type="submit">Add Product</button>
-      </form>
+    <>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-5 card p-3 mt-3 m-auto'>
+          <h2 className='mb-3'>Add Product</h2>
+          <form method="POST" onSubmit={handleSubmit} encType='multipart/form-data'>
+            <label htmlFor="">Product Title</label>
+            <input type="text" className='form-control mb-2' placeholder="Title" value={title} onChange={(e)=>setTitle(e.target.value)}/>
+            <label htmlFor="">Product Description</label>
+            <textarea type="text" className='form-control mb-2' placeholder="Description" value={description} onChange={(e)=>setDescription(e.target.value)} rows={3}></textarea>
+            <label htmlFor="">Price</label>
+            <input type="number" className='form-control mb-2' placeholder="Price" value={price} onChange={(e)=>setPrice(e.target.value)}/>
+            <label htmlFor="">Thumbnail</label>
+            <input type="file" className='form-control mb-2' placeholder="Thumbnail"  onChange={(e)=>setThumbnail(e.target.files[0])}/>
+            <button className='btn btn-info w-100 mt-3' type="submit">Add Product</button>
+          </form>
+        </div>
+      </div>
+
+
+      <div className="row mt-5">
+        <h3>Product List</h3>
+      <table className='table table-striped table-bordered table-hover'>
+        {
+          fetchProductData.map((e) => {
+        
+            return (
+              <tr key={e.id}>
+                <td>{e.title}</td>
+                <td>{e.description}</td>
+                <td>{e.price}</td>
+                <td><img src={`../../uploads/${e.thumbnail}`} width={50} className='img-fluid' alt="image" /></td>
+              </tr>
+            );
+          })
+        }
+</table>
+      </div>
     </div>
+    </>
   );
 }
 
